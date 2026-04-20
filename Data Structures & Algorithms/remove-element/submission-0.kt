39ec3ec062c7f418f374daf77fe258e1175d7973
Shubham Kumar <48342662+sbmkrgit@users.noreply.github.com>
2026-04-20 23:56:21 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun removeElement(nums: IntArray, `val`: Int): Int {
+        var count = 0
+        for(i in nums.indices){
+            if(nums[i] != `val`){
+                nums[count] = nums[i]
+                count++
+            }
+        }
+        return count
+    }
+}
