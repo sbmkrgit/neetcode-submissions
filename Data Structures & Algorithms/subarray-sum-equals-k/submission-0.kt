a2495cd8c3e48d57e788948b9c231307886792fb
Shubham Kumar <48342662+sbmkrgit@users.noreply.github.com>
2026-04-21 22:27:22 +0530

    Add: subarray-sum-equals-k - submission-0

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-0.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun subarraySum(nums: IntArray, k: Int): Int {
+        var currentSum = 0
+        var prefixSum = 0
+        var count = 0
+        val map = HashMap<Int, Int>()
+        map[0] = 1
+        for(num in  nums){
+            currentSum+=num
+            count+= map.getOrDefault(currentSum - k, 0)
+            map[currentSum] = map.getOrDefault(currentSum, 0) + 1 
+        }
+        return count
+        
+    }
+}
